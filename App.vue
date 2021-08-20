8e424c9f19ed982852636c8c5a9fb3e58dddd129
Akash G <68484085+AGprofessional@users.noreply.github.com>
2021-08-20 15:55:16 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>Vue Crash Course</h1>
+  <TodoList />
+</div>
+
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue"
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  }
+}
+</script>
+
+<style>
+#body {
+  background: rgb(66,66,66);
+  color: aliceblue;
+}
+</style>
---
+++ TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+    <h1>TodoList</h1>
+</template>
+
+{https://www.youtube.com/watch?v=PWgCqzmi0TQ&list=PLRAV69dS1uWTpHQgiV4rZFlnuS8XDl71A&index=4
+}
+
+<script>
+export default{
+data(){
+    return {
+        todos: [
+            {todoString: "todo1", completed: true},
+             {todoString: "todo2", completed: true},
+              {todoString: "todo3", completed: true},
+               {todoString: "todo4", completed: true},
+            
+        ]
+    }
+},
+methods: {
+    addTodo(newTodo){
+        this.todos.push({
+            todoString: newTodo, completed: false
+        })
+        },
+    toggleTodo(todo){
+        todo.completed = !todo.completed
+    },
+    editTodo(todo, newTodoString){
+        todo.todoString =newTodoString;
+
+    },
+    deleteTodo(deleteTodo){
+        this.todos = this.todos.filter(todo => todo !== deleteTodo)
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
